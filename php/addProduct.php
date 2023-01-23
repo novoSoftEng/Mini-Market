cf46017582f7342abebdc29da916375d7bdcfeb1
adnan el hayani <adnanelhayani12@gmail.com>
2023-01-23 13:37:07 +0100

    merge from branch adnan

--- php/addProduct.php
+++ php/addProduct.php
@@ -1,4 +1,8 @@
 <?php
+
+
+
+
 require_once('conn.php');
 $nom=$_POST['nom'];
 $description=$_POST['description'];
@@ -9,7 +13,9 @@ $targetDir = "uploads/";
 $fileName = basename($_FILES["image"]["name"]);
 $targetFilePath = $targetDir . $fileName;
 $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
-move_uploaded_file($_FILES["image"]["tmp_name"], $targetFilePath);
+move_uploaded_file($_FILES['image']['tmp_name'], "../vetement/".$_FILES['image']['name']);
+
+// move_uploaded_file($_FILES["image"]["tmp_name"], $targetFilePath);
 $image=$fileName;
 
 $query = $conn->prepare('INSERT INTO produit (nom , prix ,description ,image,quantite) VALUES(? , ? ,?,?,?)');
