c0adcd869520aa55bcfec0bd551873efd72f7eb4
chihab <hellserc2@gmail.com>
2023-01-29 13:51:35 +0000

    session

--- php/product_details.php
+++ php/product_details.php
@@ -1,13 +1,14 @@
 <?php
-
-if(isset($_POST['idpr'])){
     require("conn.php"); 
+    session_start();
+if(isset($_POST['idpr'])){
+
     $stm = $conn->prepare("SELECT * FROM produit WHERE id_pr=?");
     $stm->bindValue(1, $_POST['idpr']);
     $stm->execute();
     $row = $stm->fetch(PDO::FETCH_ASSOC);  
 
-    session_start();
+
     $_SESSION['row'] = $row;
     header("Location: /Mini-Market/php/details.php");
 }
